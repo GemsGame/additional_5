1dac3877690b1b0a73c7abe7c5b2fc84118576ed
GemsGame <support@gems.cash>
2018-09-29 16:05:14 +0300

    bad job... need more time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+    
+   
+    
+    
+
+  if (str.length % 2 > 0 ) {
+         
+      
+   
+     
+       return false;
+      
+  } else  {
+      
+      return true;
+  }
+
 }
